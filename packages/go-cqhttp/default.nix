5038a1937b75ccfaec59de3b8d4d765b7d157da6
Anillc <i@anillc.cn>
2021-11-30 19:24:02 +0800

    fix gocq

--- packages/go-cqhttp/default.nix
+++ packages/go-cqhttp/default.nix
@@ -1,20 +1,12 @@
-{ pkgs, ... }: pkgs.stdenv.mkDerivation {
+{ pkgs, ... }: pkgs.buildGo117Module {
     name = "go-cqhttp";
+    vendorSha256 = "sha256-tL/U8JYe57FahjxwgR8Bz2u+UOeguUKCv0F9Sv1BO8Q=";
     src = fetchTarball {
         url = "https://github.com/Mrs4s/go-cqhttp/archive/refs/tags/v1.0.0-beta8-fix1.tar.gz";
         sha256 = "sha256:010q7ryvgjdc7a7rzplfbcyl54vn4f98yrlnqpn3w33ijw05yks2";
     };
-    #vendorSha256 = "sha256-FOC1jkg3Iyi2b2WeiWf5KG9X6StaJHczp/F4WKADeB4="; 
-    preBuildPhase = ''
+    preBuild = ''
         substituteInPlace ./global/codec.go       --replace '"ffmpeg"' '"${pkgs.ffmpeg}/bin/ffmpeg"'
-        substituteInPlace ./global/codec/codec.go --replace '"ffmpeg"' '"${pkgs.ffmpeg}/bin/ffmpeg"'
-    '';
-    buildPhase = ''
-        HOME=$TMPDIR
-        ${pkgs.go_1_17}/bin/go build -o output/go-cqhttp .
-    '';
-    installPhase = ''
-        mkdir -p $out/bin
-        cp output/go-cqhttp $out/bin/go-cqhttp
+        substituteInPlace ./modules/silk/codec.go --replace '"ffmpeg"' '"${pkgs.ffmpeg}/bin/ffmpeg"'
     '';
 }
