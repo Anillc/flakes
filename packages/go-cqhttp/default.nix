091961260cf2f9242d8d0f282426ba02236aed5e
Anillc <i@anillc.cn>
2021-12-02 18:17:37 +0800

    rollback go-cqhttp

--- packages/go-cqhttp/default.nix
+++ packages/go-cqhttp/default.nix
@@ -1,13 +1,12 @@
 { pkgs, ... }: pkgs.buildGo117Module {
     name = "go-cqhttp";
-    vendorSha256 = "sha256-NV3IYgTFaSHxlKb17w4WzEhwX13MJIZX1vkBqdu/eOg=";
-    src = pkgs.fetchgit {
-        url = "https://github.com/Mrs4s/go-cqhttp.git";
-        rev = "e990860632783ffd39f461c081a83922b6db438e";
-        sha256 = "sha256-rbXPnJDjOAlaYm0T1PoPTA0jnYpzQhpZcTVLwGdLfeE=";
+    vendorSha256 = "sha256-wCCR3x5icPW1TZpznU9tHN8raB2SIV7Zc0PN1K8ekzA=";
+    src = fetchTarball {
+        url = "https://github.com/Mrs4s/go-cqhttp/archive/refs/tags/v1.0.0-beta7-fix2.tar.gz";
+        sha256 = "sha256:1g702xw3acpa2kaz0nzwq7kszwqqz7zrfmj9i4w9jnk1xhbwys62";
     };
     preBuild = ''
         substituteInPlace ./global/codec.go       --replace '"ffmpeg"' '"${pkgs.ffmpeg}/bin/ffmpeg"'
-        substituteInPlace ./modules/silk/codec.go --replace '"ffmpeg"' '"${pkgs.ffmpeg}/bin/ffmpeg"'
+        substituteInPlace ./global/codec/codec.go --replace '"ffmpeg"' '"${pkgs.ffmpeg}/bin/ffmpeg"'
     '';
 }
