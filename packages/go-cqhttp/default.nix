15fc452202606d89389c3a81cefa16c798bf915d
Anillc <i@anillc.cn>
2022-02-26 23:53:29 +0800

    update gocq

--- packages/go-cqhttp/default.nix
+++ packages/go-cqhttp/default.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }: pkgs.buildGo117Module {
     name = "go-cqhttp";
-    vendorSha256 = "sha256-YHF4dNuCcfYlRm4rP1SlZsIpvS9/REWQDcv7BvcNq54=";
+    vendorSha256 = "sha256-9RJTl5GZAiLY1vqhxxO6XIFxKD+1QyoMBxhzceHzelg=";
     src = fetchTarball {
-        url = "https://github.com/Mrs4s/go-cqhttp/archive/refs/tags/v1.0.0-beta8-fix2.tar.gz";
-        sha256 = "sha256:0nsdq31c69qcgbx3qmbbrkp558s67ysrn93x1arzcrvcs1bwxqag";
+        url = "https://github.com/Mrs4s/go-cqhttp/archive/refs/tags/v1.0.0-rc1.tar.gz";
+        sha256 = "sha256:1w1l5lryg7m1y3dbcjh3b8j93imr21m5hxmlzcjkx9sd14w4jzsv";
     };
     preBuild = ''
         substituteInPlace ./global/codec.go       --replace '"ffmpeg"' '"${pkgs.ffmpeg}/bin/ffmpeg"'
