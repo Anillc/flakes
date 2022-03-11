3a0c9be1b0aee5382a854ac2fe6fbc8b05bbde4a
Anillc <i@anillc.cn>
2022-03-11 16:50:16 +0800

    update babelweb2

--- packages/babelweb2/default.nix
+++ packages/babelweb2/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: pkgs.buildGoModule {
     name = "babelweb2";
-    vendorSha256 = "sha256-b3Ee2GMa8fhvDBBJtd03oNml6HyOj51iLS68eh8uDv0=";
+    vendorSha256 = "sha256-cot49hMuFv5mnxCINBFKiF2N9ktjkFzexAX5kULfdUI=";
     src = pkgs.fetchgit {
         url = "https://github.com/Anillc/babelweb2.git";
         rev = "ea898cf65663c61166df9e4caf6d9b1542cefc84";
