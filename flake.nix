23bfd4934370edf38d787adce7687610f3194b6e
Anillc <i@anillc.cn>
2022-03-11 16:12:38 +0800

    add random-src

--- flake.nix
+++ flake.nix
@@ -5,18 +5,22 @@
         url = "github:numtide/flake-utils";
         inputs.nixpkgs.follows = "nixpkgs";
     };
+    inputs.random-src = {
+        url = "github:Anillc/random-src";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+    outputs = { self, nixpkgs, flake-utils, random-src }: flake-utils.lib.eachDefaultSystem (system: let
         apps = import ./apps pkgs;
         nixosModule = import ./modules self.overlay.${system};
         pkgs = import nixpkgs { inherit system; overlays = [
             overlay
-            (self: super: { inherit flake-utils; })
+            (self: super: { inherit flake-utils; inherit (random-src.packages.${system}) random-src; })
         ]; };
         inherit (import ./packages) overlay;
     in {
         packages = {
-            inherit (pkgs) go-cqhttp bird-lg-go bird-lg-go-frontend route-chain babelweb2 babelweb2-static;
+            inherit (pkgs) go-cqhttp bird-lg-go bird-lg-go-frontend route-chain babelweb2 babelweb2-static random-src;
         };
         inherit overlay apps nixosModule;
     });
