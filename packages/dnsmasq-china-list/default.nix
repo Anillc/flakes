bbe37d202ac3f1968b8598a7d6495983f59973bf
Anillc <i@anillc.cn>
2022-07-30 00:49:42 +0800

    add dnsmasq-china-list

---
+++ packages/dnsmasq-china-list/default.nix
@@ -0,0 +1,19 @@
+{ lib, stdenv, fetchFromGitHub, ... }:
+
+with builtins;
+with lib;
+
+stdenv.mkDerivation {
+    name = "dnsmasq-china-list";
+    src = fetchFromGitHub {
+        owner = "felixonmars";
+        repo = "dnsmasq-china-list";
+        rev = "efc1171149fc9df0e0e50385bd019239fd90d28a";
+        sha256 = "sha256-P1daglWwmbJ3h39v/frtrmz1FpsyF8gbk2CFkodAv7k=";
+    };
+    buildPhase = ''
+        mkdir -p $out
+        make smartdns
+        cp *.smartdns.conf $out
+    '';
+}
