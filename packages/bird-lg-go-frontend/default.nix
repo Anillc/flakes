cc2a39e0751290a31da2ede0474b892c7d1b6ca0
Anillc <i@anillc.cn>
2022-12-12 16:07:49 +0800

    update bird-lg-go

--- packages/bird-lg-go-frontend/default.nix
+++ packages/bird-lg-go-frontend/default.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }: pkgs.buildGo117Module {
+{ buildGoModule, fetchgit, ... }: buildGoModule {
     name = "bird-lg-go";
     doCheck = false;
     vendorSha256 = "sha256-WKuVGiSV5LZrJ8/672TRN6tZNQxdCktHV6nx0ZxCP4A=";
-    src = "${pkgs.fetchgit {
+    src = "${fetchgit {
         url = "https://github.com/xddxdd/bird-lg-go.git";
         rev = "348295b9aa954a92df2cf6b1179846a9486dafc0";
         sha256 = "sha256-2t8ZP9Uc0sJlqWiJMq3MVoARfMKsuTXJkuOid0oWgyY=";
--- packages/bird-lg-go/default.nix
+++ packages/bird-lg-go/default.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }: pkgs.buildGo117Module {
+{ buildGoModule, fetchgit, ... }: buildGoModule {
     name = "bird-lg-go";
     vendorSha256 = "sha256-7LZeCY4xSxREsQ+Dc2XSpu2ZI8CLE0mz0yoThP7/OO4=";
-    src = "${pkgs.fetchgit {
+    src = "${fetchgit {
         url = "https://github.com/xddxdd/bird-lg-go.git";
         rev = "348295b9aa954a92df2cf6b1179846a9486dafc0";
         sha256 = "sha256-2t8ZP9Uc0sJlqWiJMq3MVoARfMKsuTXJkuOid0oWgyY=";
