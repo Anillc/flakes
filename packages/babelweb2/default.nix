b7dea21153c510bbd4e8e98c8693df7ab25ba76b
Anillc <i@anillc.cn>
2024-06-15 19:32:18 +0800

    remove unused packages

--- packages/babelweb2/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: pkgs.buildGoModule {
-    name = "babelweb2";
-    vendorSha256 = "sha256-cot49hMuFv5mnxCINBFKiF2N9ktjkFzexAX5kULfdUI=";
-    src = pkgs.fetchgit {
-        url = "https://github.com/Anillc/babelweb2.git";
-        rev = "ea898cf65663c61166df9e4caf6d9b1542cefc84";
-        sha256 = "sha256-sLDgI9DJS5FopfRL7tu86nhTRqpy8XeY3FTpAVGMi1o=";
-    };
-}
--- packages/bird-lg-go/default.nix
+++
@@ -1,9 +0,0 @@
-{ buildGoModule, fetchgit, ... }: buildGoModule {
-    name = "bird-lg-go";
-    vendorSha256 = "sha256-7LZeCY4xSxREsQ+Dc2XSpu2ZI8CLE0mz0yoThP7/OO4=";
-    src = "${fetchgit {
-        url = "https://github.com/xddxdd/bird-lg-go.git";
-        rev = "348295b9aa954a92df2cf6b1179846a9486dafc0";
-        sha256 = "sha256-2t8ZP9Uc0sJlqWiJMq3MVoARfMKsuTXJkuOid0oWgyY=";
-    }}/proxy";
-}
