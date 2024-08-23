b21f42250b533b9ef11ecfd49e05947143699fd0
Anillc <i@anillc.cn>
2024-08-23 12:05:54 +0800

    replace flake-utils with flake.parts

--- template/flake.nix
+++ template/flake.nix
@@ -1,9 +1,12 @@
 {
-  outputs = {
-    self, nixpkgs, flake-utils,
-  }: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.default = pkgs.mkShell {};
-  });
+  outputs = inputs@{
+    self, nixpkgs, flake-parts,
+  }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    perSystem = { pkgs, ... }: {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [];
+      };
+    };
+  };
 }
