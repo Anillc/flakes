4f708f16d6c3c8bc323c088ba653ba96a44c4871
Anillc <i@anillc.cn>
2024-10-14 16:50:41 +0800

    update agda-lanaguage-server

--- packages/agda-language-server/default.nix
+++ packages/agda-language-server/default.nix
@@ -12,10 +12,10 @@ autoPatchelfHook,
 
 stdenv.mkDerivation rec {
   pname = "als";
-  version = "v0.2.6.3.0";
+  version = "v0.2.6.4.0.3";
   src = fetchurl {
     url = "https://github.com/banacorn/agda-language-server/releases/download/${version}/als-ubuntu.zip";
-    sha256 = "sha256-1msmuC/zjSJ+gP9ed/Eh7D7rdKSoIfg8FlywSDbjvag=";
+    sha256 = "sha256-nyQSbNxCloaV2TX3dR7DVfL56wVeCkRpq4zHFZpsg+o=";
   };
   nativeBuildInputs = [ autoPatchelfHook unzip ];
   buildInputs = [ zlib icu70 ncurses gmp ];
