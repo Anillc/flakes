e9d83ee913889c0db87653b57cca7b40c8096932
Anillc <i@anillc.cn>
2025-01-22 22:35:23 +0800

    update template

--- template/flake.nix
+++ template/flake.nix
@@ -5,6 +5,7 @@
     systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     perSystem = { pkgs, ... }: {
       devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [];
         nativeBuildInputs = with pkgs; [];
       };
     };
